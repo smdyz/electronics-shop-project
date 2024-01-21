9b98ff81de363e190ae74a0700d431c36bee7086
Tim Zaplavnyy <tima.zaplavnyy@mail.ru>
2024-01-21 13:45:12 +0300

    done fourth homework

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,16 +1,20 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 import pytest
 from src.item import Item
+from src.phone import Phone
 
 item1 = Item("Смартфон", 10000, 20)
+phone1 = Phone("iPhone 14", 120_000, 5, 2)
 
 
 def test_repr():
     assert repr(item1) == "Item('Смартфон', 10000, 20)"
+    assert repr(phone1) == "Phone('iPhone 14', 120000, 5, 2)"
 
 
 def test_str():
     assert str(item1) == 'Смартфон'
+    assert str(phone1) == 'iPhone 14'
 
 
 def test_calculate_total_price():
@@ -22,6 +26,10 @@ def test_apply_discount():
     assert item1.apply_discount() == 5000
 
 
+def test_string_to_num():
+    assert Item.string_to_number('5.0') == 5
+
+
 def test_name_getter():
     assert item1.name == 'Смартфон'
 
@@ -30,3 +38,17 @@ def test_name_setter():
     assert item1.name == 'Смартфон'
     item1.name = 'СуперСмартфон'
     assert item1.name == 'СуперСмарт'
+
+
+def test_add():
+    assert item1 + phone1 == 25
+    assert phone1 + 1 is None
+
+
+def test_number_of_sim():
+    assert phone1.number_of_sim == 2
+    phone1.number_of_sim = 1
+    assert phone1.number_of_sim == 1
+    with pytest.raises(ValueError):
+        phone1.number_of_sim = 0
+
