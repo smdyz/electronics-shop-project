6efab5bdb1ac98f0c8292000c84e4ea60317b356
Tim Zaplavnyy <tima.zaplavnyy@mail.ru>
2024-01-22 23:24:58 +0300

    fixed fifth homework

--- src/keyboard.py
+++ src/keyboard.py
@@ -1,19 +1,31 @@
 from src.item import Item
 
 
-class Keyboard(Item):
+class LanguageMixin:
+    __ch_l_flag = False
 
-    def __init__(self, name: str, price: float, quantity: int):
+    def __init__(self, name, price, quantity):
         super().__init__(name, price, quantity)
-        self.language = "EN"
-        self.__ch_l_flag = False
+        self.__language = "EN"
+
+    @property
+    def language(self):
+        return self.__language
 
     def change_lang(self) -> None:
         if not self.__ch_l_flag:
             self.__ch_l_flag = True
-            self.language = "RU"
+            self.__language = "RU"
             return None
         if self.__ch_l_flag:
             self.__ch_l_flag = False
-            self.language = "EN"
+            self.__language = "EN"
             return None
+
+
+class Keyboard(LanguageMixin, Item):
+
+    def __init__(self, name: str, price: float, quantity: int):
+        super().__init__(name, price, quantity)
+        self.__ch_l_flag = False
+
